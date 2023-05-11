7023a8128e7bdd0a36d855a9e3017d39bab48aac
Iara Tassi <iaratassi@hotmail.com>
2023-05-11 09:14:38 -0300

    Adicionando testes

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "h6aw9g",
   e2e: {
+    baseUrl: 'http://automationpratice.com.br/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/exemplo.cy.js
+++ cypress/e2e/exemplo.cy.js
@@ -1,7 +1,7 @@
 describe('Login', () => {
     it('Login com sucesso', () => {
         // acessar a minha aplicação
-        cy.visit('http://automationpratice.com.br/')
+        cy.visit('/')
             .get('#top_header')
 
         // crir os steps/ações
@@ -13,7 +13,69 @@ describe('Login', () => {
         // Click em um elemento
         cy.get('.clear .theme-btn-one.btn_md')
             .click()
+
+        cy.get('.swal2-confirm') 
+            .click()  
+            
+        // #mc_enbed_signup > form > div > div. button
+        // .clear .theme-btn-one.btn_md  
     })
 
    
 });
+
+    describe('Novidades', () => {
+        it('Cadastrar e-mail com sucesso', () => {
+        
+            cy.visit('/')
+                .get('#top_header')
+
+            cy.get('.form-control')
+                .type('dud@qazando.com.br')
+
+            cy.get('.clear .theme-btn-one.btn_md')
+                .click()
+
+            cy.get('.swal2-title')
+                .should('be.visible')    
+                .should('have.text', 'Success')
+                        
+            
+        })
+
+        it('Cadastrar e-mail com sucesso apertando enter', () => {
+            cy.visit('/')
+                .get('#top_header')
+
+            cy.get('.form-control')
+                .type('dud@qazando.com.br{enter}')
+
+            cy.get('.swal2-title')
+                .should('be.visible')    
+                .should('have.text', 'Success')
+                        
+            
+        })
+
+        it('Verificar fechamento da mensagem de sucesso', () => {
+            cy.visit('/')
+                
+            cy.get('.form-control')
+                .type('dud@qazando.com.br')
+
+            cy.get('.clear .theme-btn-one.btn_md')
+                .click()
+
+            cy.get('.swal2-actions > .swal2-confirm')
+                .click()
+
+            cy.wait(1000) 
+            
+            cy.get('#swal2-title')
+                .should('not.exist')    
+                                
+            
+        })
+
+    
+    });
